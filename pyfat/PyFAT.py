add04091bee17a7e5f054d01e48de88ccb6624a6
Nathan-J. Hirschauer <nathanhi@deepserve.info>
2018-12-29 04:18:56 +0100

    FREE_CLUSTER only is an EOC marker if a chain has actually been followed

--- pyfat/PyFAT.py
+++ pyfat/PyFAT.py
@@ -402,6 +402,10 @@ class PyFAT:
                 raise PyFATException("Bad cluster found in FAT cluster "
                                      "chain, cannot access file")
             elif self.fat[i] == self.FAT_CLUSTER_VALUES[self.fat_type]["FREE_CLUSTER"]:
+                if i == first_cluster:
+                    # Cluster chain may not start with a free cluster marker
+                    return
+
                 # FREE_CLUSTER mark when following a chain is treated as EOC
                 yield address
                 return
