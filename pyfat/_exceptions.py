6559b4aab87325d4edeac1d2cf5fa1c5fcbb7658
Nathan-J. Hirschauer <nathanhi@deepserve.info>
2019-10-25 17:48:40 +0200

    Add docstrings to _exceptions.py and fix flake8 issues

--- pyfat/_exceptions.py
+++ pyfat/_exceptions.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 
-"""Custom Exceptions for PyFAT"""
+"""Custom Exceptions for PyFAT."""
 
 
 class PyFATException(Exception):
-    """Generic PyFAT Exceptions"""
-    def __init__(self, msg, errno=None):
+    """Generic PyFAT Exceptions."""
+
+    def __init__(self, msg: str, errno=None):
+        """Construct base class for PyFAT exceptions.
+
+        :param msg: Exception message describing what happened
+        :param errno: Error number, mostly based on POSIX errno where feasible
+        """
         Exception.__init__(self, msg)
         self.errno = errno
 
 
 class NotAnLFNEntryException(PyFATException):
+    """Indicates that given dir entry cannot be interpreted as LFN entry."""
+
     pass
