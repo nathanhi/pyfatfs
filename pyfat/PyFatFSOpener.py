407032620d560a5b1dcce9bf6a4fc5405f4c71fd
Nathan-J. Hirschauer <nathanhi@deepserve.info>
2019-10-25 18:01:24 +0200

    Add docstrings to PyFatFSOpener.py and fix flake8 issues

--- pyfat/PyFatFSOpener.py
+++ pyfat/PyFatFSOpener.py
@@ -1,5 +1,7 @@
-# coding: utf-8
-"""Defines the PyFatFSOpener."""
+# -*- coding: utf-8 -*-
+"""Registers the PyFatFSOpener used by PyFilesystem2."""
+
+from fs.opener.parse import ParseResult
 
 __all__ = ['PyFatFSOpener']
 
@@ -9,9 +11,13 @@ from pyfat.PyFatFS import PyFatFS
 
 
 class PyFatFSOpener(Opener):
+    """Registers fat:// protocol for PyFilesystem2."""
+
     protocols = ['fat']
 
-    def open_fs(self, fs_url, parse_result, writeable, create, cwd):
+    def open_fs(self, fs_url: str, parse_result: ParseResult,
+                create: bool, cwd: str, writeable: bool = True):
+        """Handle PyFilesystem2's protocol opening interface."""
         kwargs = {'offset': parse_result.params.get('offset', 0)}
         if parse_result.params.get('encoding', None) is not None:
             kwargs['encoding'] = parse_result.params.get('encoding')
