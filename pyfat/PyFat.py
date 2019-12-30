05884b756b60ebdfbf0aef2fd959bb77003c520f
Nathan-J. Hirschauer <nathanhi@deepserve.info>
2019-12-30 19:15:09 +0100

    Adjust MAX_DATA_CLUSTER values to standard conformity

--- pyfat/PyFat.py
+++ pyfat/PyFat.py
@@ -48,7 +48,7 @@ class PyFat(object):
     #: Possible cluster values for FAT12 partitions
     FAT12_CLUSTER_VALUES = {'FREE_CLUSTER': 0x000,
                             'MIN_DATA_CLUSTER': 0x002,
-                            'MAX_DATA_CLUSTER': 0xFF5,
+                            'MAX_DATA_CLUSTER': 0xFEF,
                             'BAD_CLUSTER': 0xFF7,
                             'END_OF_CLUSTER_MIN': 0xFF8,
                             'END_OF_CLUSTER_MAX': 0xFFF}
@@ -56,14 +56,14 @@ class PyFat(object):
     #: Possible cluster values for FAT16 partitions
     FAT16_CLUSTER_VALUES = {'FREE_CLUSTER': 0x0000,
                             'MIN_DATA_CLUSTER': 0x0002,
-                            'MAX_DATA_CLUSTER': 0xFFF5,
+                            'MAX_DATA_CLUSTER': 0xFFEF,
                             'BAD_CLUSTER': 0xFFF7,
                             'END_OF_CLUSTER_MIN': 0xFFF8,
                             'END_OF_CLUSTER_MAX': 0xFFFF}
     #: Possible cluster values for FAT32 partitions
     FAT32_CLUSTER_VALUES = {'FREE_CLUSTER': 0x0000000,
                             'MIN_DATA_CLUSTER': 0x0000002,
-                            'MAX_DATA_CLUSTER': 0xFFFFFF5,
+                            'MAX_DATA_CLUSTER': 0x0FFFFFEF,
                             'BAD_CLUSTER': 0xFFFFFF7,
                             'END_OF_CLUSTER_MIN': 0xFFFFFF8,
                             'END_OF_CLUSTER_MAX': 0xFFFFFFF}
