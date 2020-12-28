1eda0a82767e381c6aa298e0a8e02a0511a9470f
Nathan-J. Hirschauer <nathanhi@deepserve.info>
2020-12-28 20:44:25 +0100

    Raise IOError when writing to a read-only file handle

--- pyfat/FatIO.py
+++ pyfat/FatIO.py
@@ -151,6 +151,10 @@ class FatIO(io.RawIOBase):
 
     def write(self, __b: Union[bytes, bytearray]) -> Optional[int]:
         """Write given bytes to file."""
+
+        if not self.writable():
+            raise IOError('Cannot write to read-only file!')
+
         sz = len(__b)
         cluster = self.dir_entry.get_cluster()
         if sz == 0:
